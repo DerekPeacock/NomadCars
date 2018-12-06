74d097dcd2a9f91c9a36bc02943b55d0f7d8cb5d
Connor Patey <21701762@bucks.ac.uk>
2018-12-06 14:11:13 +0000

    updated payment and purchase

--- NomadCars/Models/PaymentCard.cs
+++ NomadCars/Models/PaymentCard.cs
@@ -1,14 +1,26 @@
 using System;
 public class PaymentCard
 {
-	private int cardID;
-	private int cardType;
-	private String cardNumber;
-	private String nameOnCard = "0 -100%";
-	private short securityNumber;
-	private int expiryMonth;
-	private int expiryYear;
+	public int CardID { get; set; }
+    //A specific ID used to identify each card uniquely
 
-	private Person person;
+    public string CardType { get; set; }
+    //This is the type of card that the customer will be using to make their purchase
+
+    public String CardNumber { get; set; }
+    //A unique 16 digit number on the customer's card
+
+    public String NameOnCard { get; set; }
+    //This will be the name of the card owner
+
+    public short SecurityNumber { get; set; }
+    //The 3 digit number on the customer's card
+
+    public int ExpiryMonth { get; set; }
+    //The month in which the card expires
+
+    public int ExpiryYear { get; set; }
+    //The year that the card expires
+
 
 }
--- NomadCars/Models/Purchase.cs
+++ NomadCars/Models/Purchase.cs
@@ -4,30 +4,44 @@ using System;
 /// </summary>
 public class Purchase {
 	/// <summary>
-	/// A specific number given to each invoice in order for each to be identified within the database for the purposes of tracking or record keeping, i.e. for end of week or annual car sales.
+	/// A specific number given to each invoice in order for each to be identified within the database for the 
+    /// purposes of tracking or record keeping, i.e. for end of week or annual car sales.
 	/// </summary>
-	private int invoiceNumber;
-	/// <summary>
-	/// A detailed receipt of the purchase made by the customer, including the product, amount, address of the customer, address of the seller as well as the date of payment.
-	/// </summary>
-	private String invoice;
-	/// <summary>
-	/// The document, held by the sales team, for the customer to complete in order for the car to be registered in their name, as per the law of the government.
-	/// </summary>
-	private bool vehicleRegistrationDocument;
-	/// <summary>
-	/// A record of the database representing whether or not the customer has returned the car. This will be allowed within a 30 day period of time.
-	/// </summary>
-	private bool return_;
-	/// <summary>
-	/// The cost of the car returned to the customer as part of the refund procedure. Note this only applies if the customer requests a refund due to a fault discovered in the car before the 30 day time period has concluded.
-	/// </summary>
-	private int refund;
-	private BigDecimal intrest;
-	private String transactionType;
+	public string InvoiceNumber { get; set; }
+/// <summary>
+/// A detailed receipt of the purchase made by the customer, including the product, 
+/// amount, address of the customer, address of the seller as well as the date of payment.
+/// </summary>
+public bool Invoice { get; set; }
+    /// <summary>
+    /// The document, held by the sales team, for the customer to complete in order for the 
+    /// car to be registered in their name, as per the law of the government.
+    /// </summary>
+    public bool VehicleRegistrationDocument { get; set; }
+    /// <summary>
+    /// A record of the database representing whether or not the customer has returned the car. 
+    /// This will be allowed within a 30 day period of time.
+    /// </summary>
+    public bool Return_ { get; set; }
+    /// <summary>
+    /// The cost of the car returned to the customer as part of the refund procedure. 
+    /// Note this only applies if the customer requests a refund due to a fault discovered in the car before the 30 day time period has concluded.
+    /// </summary>
+    public int Refund { get; set; }
 
-	private Car car;
-	private Person person;
-	private Staff staff;
+    public decimal Intrest { get; set; }
+    //Nullable
+    public String TransactionType { get; set; }
+
+    public int PersonID { get; set; }
+
+    public int StaffID { get; set; }
+
+    public int CarID { get; set; }
+
+    public virtual Car Car { get; set; }
+
+    public virtual Person Person { get; set; }
+    public virtual Staff Staff { get; set; }
 
 }
