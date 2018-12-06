0df11786e6bd96cb782f1d708c83120e71dfc442
DerekPeacock <derek.peacock@yahoo.com>
2018-12-06 14:04:07 +0000

    Updated staff class

--- NomadCars/Models/Staff.cs
+++ NomadCars/Models/Staff.cs
@@ -1,27 +1,40 @@
 using System;
-/// <summary>
-/// Daniel
-/// </summary>
-public class Staff {
-	/// <summary>
-	/// A specific identification number that identifies the member of staff within the database.
-	/// </summary>
-	private int staffID;
-	/// <summary>
-	/// The department that the member of staff works for. The departments include sales, database administration and management
-	/// </summary>
-	private String department;
-	/// <summary>
-	/// The specific position that the member of staff holds within their department, e.g. sales representative or database administrator.
-	/// </summary>
-	private String jobPosition;
-	private BigDecimal salary;
-	private Integer startDate;
-	private String imageURL;
-	private String staffDescription;
+namespace NomadCars.Models
+{
+    /// <summary>
+    /// Daniel's Class
+    /// Each Member of staff is a Person working for Nomad Cars
+    /// </summary>
+    public class Staff
+    {
+        /// <summary>
+        /// A specific identification number that identifies the member 
+        /// of staff within the database.
+        /// </summary>
+        public int StaffID { get; set; }
+        /// <summary>
+        /// The department that the member of staff works for. 
+        /// The departments include sales, database administration and management
+        /// </summary>
+        public String Department { get; set; }
+        /// <summary>
+        /// The specific position that the member of staff holds within 
+        /// their department, e.g. sales representative or database administrator.
+        /// </summary>
+        public String JobPosition { get; set; }
 
-	private Purchase purchase;
+        public decimal Salary { get; set; }
 
-	private Person person;
+        public int StartDate { get; set; }
 
+        public String ImageURL { get; set; }
+
+        public String Description { get; set; }
+
+        // Foriegn Key
+        public int PersonID { get; set; }
+
+        public virtual Person Person { get; set; }
+
+    }
 }
