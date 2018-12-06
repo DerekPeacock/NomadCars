02cbc4abdcfc4386d689d0d477fdb4a0f3d0b39e
William Foster <William_1foster@yahoo.co.uk>
2018-12-06 14:34:57 +0000

    Corrections made to Person

--- NomadCars/Models/Person.cs
+++ NomadCars/Models/Person.cs
@@ -1,44 +1,49 @@
 using System;
-/// <summary>
-/// William
-/// </summary>
-public class Person {
-	public int PersonID { get; set; }
-	/// <summary>
-	/// The Forename of any given person.
-	/// </summary>
-	public String FirstName { get; set; }
-	/// <summary>
-	/// The surname of any given person.
-	/// </summary>
-	public String LastName { get; set; }
+
+namespace NomadCars.Models
+{
     /// <summary>
-    /// 
+    /// William
     /// </summary>
-    public int HomePhoneNumber { get; set; }
-    /// <summary>
-    /// The phone number any given person, by which they can be contacted with.
-    /// </summary>
-    public int MobilePhoneNumber { get; set; }
-	/// <summary>
-	/// The e-mail held by a person by which they can be contacted
-	/// </summary>
-	public String Email { get; set; }
-	/// <summary>
-	/// The date of birth of the person
-	/// </summary>
-	public Date DateOfBirth { get; set; }
-	/// <summary>
-	/// The is a current statement of someone's personal relationship status, e.g. single, married or divorced. This kind of information is often used in forms to determine details such as financial needs.
-	/// </summary>
-	public String MaritalStatus { get; set; }
-	public bool Customer { get; set; }
-	
+    public class Person
+    {
+        public int PersonID { get; set; }
+        /// <summary>
+        /// The Forename of any given person.
+        /// </summary>
+        public String FirstName { get; set; }
+        /// <summary>
+        /// The surname of any given person.
+        /// </summary>
+        public String LastName { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int HomePhoneNumber { get; set; }
+        /// <summary>
+        /// The phone number any given person, by which they can be contacted with.
+        /// </summary>
+        public int MobilePhoneNumber { get; set; }
+        /// <summary>
+        /// The e-mail held by a person by which they can be contacted
+        /// </summary>
+        public String Email { get; set; }
+        /// <summary>
+        /// The date of birth of the person
+        /// </summary>
+        public Date DateOfBirth { get; set; }
+        /// <summary>
+        /// The is a current statement of someone's personal relationship status, e.g. single, married or divorced. This kind of information is often used in forms to determine details such as financial needs.
+        /// </summary>
+        public String MaritalStatus { get; set; }
+        public bool Customer { get; set; }
 
-	public Staff staff;
-	public Address[] address;
-	public Purchase purchase;
 
-	public _Payment_Card card;
+        public Staff staff;
+        public Address[] address;
+        public Purchase purchase;
 
+        public _Payment_Card card;
+
+    }
 }
