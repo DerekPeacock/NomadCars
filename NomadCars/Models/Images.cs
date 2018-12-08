67f45f3ba50699a7fef68ee8d3487a5d588a719c
William Foster <William_1foster@yahoo.co.uk>
2018-12-08 11:28:16 +0000

    Compilations and annotations completed for Images class

--- NomadCars/Models/Images.cs
+++ NomadCars/Models/Images.cs
@@ -7,16 +7,45 @@ using System.ComponentModel.DataAnnotations;
 /// </summary>
 namespace NomadCars.Models
 {
-
     public class Images
     {
-        private int ImageID;
-        private String ImageURL;
-        private String Description;
-        private String Caption;
-        private String ImageType;
+        /// <summary>
+        /// A number assigned to each image as
+        /// a unique identifier.
+        /// </summary>
+        public int ImageID;
 
-        private ICollection<Car> car;
+        /// <summary>
+        /// The URL of the image being used on 
+        /// our website.
+        /// </summary>
+        [Required, StringLength(255), DataType(DataType.ImageUrl)]
+        public String ImageURL;
+
+        /// <summary>
+        /// A short description of what is being 
+        /// displayed in the image.
+        /// </summary>
+        [Required, StringLength(255), Display(Name = "Description")]
+        public String Description;
+
+        /// <summary>
+        /// A message that will appear alongside 
+        /// the image as a one sentance summary 
+        /// of the image's contents.
+        /// </summary>
+        [Required, StringLength(50)]
+        public String Caption;
+
+        /// <summary>
+        /// Refers to the positioning of the car 
+        /// from where the photo was taken, e.g. 
+        /// front, back or interior.
+        /// </summary>
+        [Required, StringLength(20)]
+        public String ImageType;
+
+        public ICollection<Car> car;
 
     }
 }
--- NomadCars/Models/Person.cs
+++ NomadCars/Models/Person.cs
@@ -15,36 +15,43 @@ namespace NomadCars.Models
         /// primarily a customer.
         /// </summary>
         public int PersonID { get; set; }
+
         /// <summary>
         /// The Forename of any given person.
         /// </summary>
         [Required, StringLength(20), Display(Name = "First Name")]
         public String FirstName { get; set; }
+
         /// <summary>
         /// The surname of any given person.
         /// </summary>
         [Required, StringLength(20), Display(Name = "Last Name")]
         public String LastName { get; set; }
+
         /// <summary>
         /// The phone number of a person, assigned to a home phone.
         /// </summary>
         [StringLength(10), DataType(DataType.PhoneNumber), Display(Name = "Home Phone Number")]
         public int HomePhoneNumber { get; set; }
+
         /// <summary>
         /// The phone number of a person, assigned to their mobile phone.
         /// </summary>
         [StringLength(10), DataType(DataType.PhoneNumber), Display(Name = "Mobile Phone NUmber")]
         public int MobilePhoneNumber { get; set; }
+
         /// <summary>
         /// The e-mail held by a person by which they can be contacted
         /// </summary>
         [Required, StringLength(10), Display(Name = "E-mail Address")]
         public String Email { get; set; }
+
         /// <summary>
         /// The date of birth of the person
         /// </summary>
         [Required, StringLength(8), DataType(DataType.DateTime), Display(Name = "dd/mm/yy")]
         public DateTime DateOfBirth { get; set; }
+
         /// <summary>
         /// The is a current statement of someone's personal relationship status, 
         /// e.g. single, married or divorced. This kind of information is often 
@@ -52,6 +59,7 @@ namespace NomadCars.Models
         /// </summary>
         [Required, StringLength(20), Display(Name = "Marital Status")]
         public String MaritalStatus { get; set; }
+
         /// <summary>
         /// A boolean statement that refers to whether or not the person 
         /// concerned is a customer.
