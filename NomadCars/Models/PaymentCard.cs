f7dd7143987bf25d1846fd97058ef8b7984a9bf8
Connor Patey <patec005@gmail.com>
2018-12-13 12:48:58 +0000

    Updated PaymentCard

--- NomadCars/Models/PaymentCard.cs
+++ NomadCars/Models/PaymentCard.cs
@@ -2,29 +2,37 @@ using System;
 
 namespace NomadCars.Models
 {
+    public enum CardTypes
+    {
+        VISA_CREDIT,
+        VISA_DEBIT,
+        MASTERCARD,
+        PAYPAL
+    }
 
     public class PaymentCard
     {
-        public int CardID { get; set; }
         //A specific ID used to identify each card uniquely
-
-        public string CardType { get; set; }
+        public int CardID { get; set; }
+        
         //This is the type of card that the customer will be using to make their purchase
-
-        public String CardNumber { get; set; }
+        public CardTypes CardType { get; set; }
+        
         //A unique 16 digit number on the customer's card
-
-        public String NameOnCard { get; set; }
+        public string CardNumber { get; set; }
+        
         //This will be the name of the card owner
-
-        public short SecurityNumber { get; set; }
+        public string NameOnCard { get; set; }
+       
         //The 3 digit number on the customer's card
-
-        public int ExpiryMonth { get; set; }
+        public short SecurityNumber { get; set; }
+        
         //The month in which the card expires
-
-        public int ExpiryYear { get; set; }
+        public int ExpiryMonth { get; set; }
+        
         //The year that the card expires
+        public int ExpiryYear { get; set; }
+        
 
 
     }
