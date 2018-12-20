e8a4605f9ab4aa5964b20f984a9150b793380d37
DerekPeacock <derek.peacock@yahoo.com>
2018-12-20 14:42:04 +0000

    Updated Payment Card and added migration

--- NomadCars/Models/PaymentCard.cs
+++ NomadCars/Models/PaymentCard.cs
@@ -1,15 +1,23 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace NomadCars.Models
 {
     public enum CardTypes
     {
+        [Display(Name="Visa Credit Card")]
         VISA_CREDIT,
+        [Display(Name = "Visa Debit Card")]
         VISA_DEBIT,
+        [Display(Name = "MasterCard")]
         MASTERCARD,
+        [Display(Name = "PayPal")]
         PAYPAL
     }
 
+    /// <summary>
+    /// Connor
+    /// </summary>
     public class PaymentCard
     {
         //A specific ID used to identify each card uniquely
@@ -17,20 +25,26 @@ namespace NomadCars.Models
         
         //This is the type of card that the customer will be using to make their purchase
         public CardTypes CardType { get; set; }
-        
+
         //A unique 16 digit number on the customer's card
+        [StringLength(20), Required]
         public string CardNumber { get; set; }
-        
+
         //This will be the name of the card owner
+        [StringLength(20), Required]
         public string NameOnCard { get; set; }
-       
+
         //The 3 digit number on the customer's card
-        public short SecurityNumber { get; set; }
+        [StringLength(3), Required, RegularExpression("[999]",
+            ErrorMessage ="Please enter 3 digits!")]
+       public string SecurityNumber { get; set; }
         
         //The month in which the card expires
+        [Range(1,12)]
         public int ExpiryMonth { get; set; }
         
         //The year that the card expires
+        [Range(2018,2024)]
         public int ExpiryYear { get; set; }
         
 
